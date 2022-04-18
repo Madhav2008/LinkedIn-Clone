4ef93a3add8a3076746b115ff129ffa2b6acd215
arora <aroramadhav65@gmail.com>
2022-04-18 20:26:02 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Linkedin',
       theme: ThemeData(
         // This is the theme of your application.
         //
